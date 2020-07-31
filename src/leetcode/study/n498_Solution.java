b73ae9837d5210c57d5dbf0628a9359491ebb0b6
lpwang <13889522960@163.com>
2020-07-31 19:10:42 +0800

    添加n498_Solution

---
+++ src/leetcode/study/n498_Solution.java
@@ -0,0 +1,42 @@
+class n498_Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+    	if (matrix.length == 0) {
+    		return new int[0];
+    	}
+    	int size = matrix.length * matrix[0].length;
+    	int index = 0;
+    	int[] result = new int[size];
+    	int maxLoop = matrix.length + matrix[0].length - 1;
+    	for (int i=0;i<maxLoop;i++) {
+    		int m,n = 0;
+    		if (i%2 == 0) {
+    			if (i < matrix.length) {
+    				m = i;
+    				n = 0
+    			} else {
+    				m = matrix.length - 1;
+    				n = i - m;
+    			}
+    			while (m >= 0 && n < matrix[0].length) {
+    				result[index++] = matrix[m][n];
+    				m--;
+    				n++;
+    			}
+    		} else {
+    			if (i < matrix[0].length) {
+    				m = 0;
+    				n = i;
+    			} else {
+    				n = matrix[0].length - 1;
+    				m = i - n;
+    			}
+    			while (n >= 0 && m < matrix.length) {
+    				result[index++] = matrix[m][n];
+    				m++;
+    				n--;
+    			}
+    		}
+    	}
+    	return result;
+    }
+}
