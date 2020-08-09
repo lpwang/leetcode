c7679c99bd75907f30d6becc2ff7a1f5c0897915
lpwang <13889522960@163.com>
2020-08-09 23:18:33 +0800

    添加寻找旋转排序数组中的最小值的二分查找题解

--- src/leetcode/study/n153_Solution.java
+++ src/leetcode/study/n153_Solution.java
@@ -12,3 +12,21 @@ class n153_Solution {
     	return minVal;
     }
 }
+
+// 二分查找求解
+
+class Solution {
+    public int findMin(int[] nums) {
+    	int l =0;
+    	int r = nums.length - 1;
+    	while (l < r) {
+    		int mid = l + (r-l)/2;
+    		if (nums[mid] > nums[r]) {
+    			l = mid + 1;
+    		} else {
+    			r = mid;
+    		}
+    	}
+    	return nums[l];
+    }
+}
